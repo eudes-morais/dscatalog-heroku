591f11cf247b9901a8e1fae45365e54968580211
Eudes Morais <eudes.morais@gmail.com>
2021-07-16 00:07:50 -0300

    Commit test

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- Utilizado o nome DATA ao invés de IMPORT, pois dá menos problema
+-- Utilizado o nome DATA ao invés de IMPORT, por causa de problemas
 
 INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Alex', 'Brown', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 INSERT INTO tb_user (first_name, last_name, email, password) VALUES ('Maria', 'Green', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
